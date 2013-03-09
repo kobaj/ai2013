5f6502de82c619bf67fee8b668936039001249f9
Tom Shehan <tshehan6@gmail.com>
2013-03-08 22:26:55 -0600

    got some more done

--- src/us/KnowledgeGraph.java
+++ src/us/KnowledgeGraph.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import spacewar2.simulator.Toroidal2DPhysics;
 import spacewar2.simulator.Toroidal2DPhysics;
 import spacewar2.utilities.Position;
+import spacewar2.utilities.Vector2D;
 import spacewar2.objects.*;
 
 // A directed path from a to b with no properties
-abstract class Relation{
+class Relation{
 	
 	// A and B - the edges this relation connects
 	protected SpacewarObject a;
 	protected SpacewarObject b;
 	
-	public abstract Relation makeRelation(SpacewarObject a, SpacewarObject b);
+	// make a relation between A and B if possible. This is the base class so it is never possible
+	public static Relation make(SpacewarObject a, SpacewarObject b, Toroidal2DPhysics space){
+		return null;
+	}
 	
 	// constructor
 	public Relation(SpacewarObject a, SpacewarObject b){
@@ -42,17 +46,48 @@ abstract class Relation{
 // An example of a relation with some properties
 class ApproachingCurrentPosition extends Relation{
 
-	public static ApproachingCurrentPosition makeRelation(SpacewarObject a, SpacewarObject b){
-		// if the conditions are right, return a relation between a and b with the appropriate
-		// properties by calling the constructor on a and b. otherwise return null.
+	// make the relation if a will approximately reach B's location in some number of steps
+	public static ApproachingCurrentPosition make(SpacewarObject a, SpacewarObject b, Toroidal2DPhysics space){
+
+		// set constants
+		int radius = 20;
+		int steps = 50;
 		
-		// obviously this is wrong. Would it be possible to just make the constructor so that 
-		// it checks the input and throws an exception if there is no such relation?
+		// get velocity vector and position for A
+		Vector2D v = a.getPosition().getTranslationalVelocity();
+		Position futurePosition = a.getPosition();
+		
+		// see if A will arrive at current position of B within given number  of frames
+		int i = 0;
+		while(i < steps){
+			
+			// return relation if approximate collision found at this step
+			if(space.findShortestDistance(b.getPosition(), futurePosition) < radius){
+				ApproachingCurrentPosition r = new ApproachingCurrentPosition(a,b);
+				r.steps(steps);
+				return r;
+			}
+			
+			// increment step and future position
+			futurePosition.setX(futurePosition.getX() + v.getXValue());
+			futurePosition.setY(futurePosition.getY() + v.getYValue());
+			++i;
+			
+		}
+		return null ;
+		
 		
 	}
 
 	// the number of steps before B occupies the approximate current position of A
-	protected double timeBeforeThere;
+	protected int steps;
+	public int steps(){
+		return steps;
+	}
+	public void steps(int steps){
+		this.steps = steps;
+	}
+	
 	
 	// the constructor
 	public ApproachingCurrentPosition(SpacewarObject a, SpacewarObject b) {
@@ -73,7 +108,23 @@ public class KnowledgeGraph{
 	ArrayList<Relation> edges;
 	
 	public KnowledgeGraph(Toroidal2DPhysics space){
-		// fill ine vertices and edges
+		
+		ArrayList<SpacewarObject> objects = new ArrayList<SpacewarObject>();
+		objects.addAll(space.getAsteroids());
+		objects.addAll(space.getBases());
+		objects.addAll(space.getBeacons());
+		objects.addAll(space.getShips());
+		objects.addAll(space.getWeapons());
+		
+		for(SpacewarObject a : objects){
+			for(SpacewarObject b : objects){
+				Relation r = ApproachingCurrentPosition.make(a, b, space);
+				if(r != null){
+					edges.add(r);
+				}
+			}
+		}
+		
 	}
 	
 	// get the list of relations between two SpacewarObjects
